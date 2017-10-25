8366279149d2a3f8a8f00042666b10d36df0bcdb
Billy Kang <billy@nurigo.net>
2017-10-25 11:50:33 +0900

    back to original structure

---
+++ frontend/src/components/pages/HomePage/HomePage.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { PageTemplate, Block } from 'components';
+import {HeaderContainer} from 'containers';
+import logo from 'static/images/logo.svg';
+
+import classNames from 'classnames/bind';
+import styles from 'static/css/App.css';
+
+const cx = classNames.bind(styles);
+
+
+const HomePage = () => {
+  return (
+    <PageTemplate header={<HeaderContainer />} >
+        <Block center shadow>
+            <img className={cx('App-logo')} src={logo} alt="logo" />
+            <h2 className={cx('App-intro')}>
+                Welcome to React Coolsms Microservice Boilerplate
+            </h2>
+            <h5> composed & organized by Billy Kang </h5>
+        </Block>
+    </PageTemplate>
+  );
+};
+
+export default HomePage;
