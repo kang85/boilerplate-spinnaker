04fbb4566340a9d58233c05ff5f60093832af213
Billy Kang <billy@nurigo.net>
2017-10-26 10:50:24 +0900

    ArsPage added to route

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
-import { HomePage } from 'components';
+import { HomePage, ArsPage } from 'components';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/ars" component={HomePage} />
+        <Route exact path="/ars" component={ArsPage} />
       </div>
     );
   }
